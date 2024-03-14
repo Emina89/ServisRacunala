4986b38360e7ba4b63a996bc42760b10ab607c1c
Emina Vejsilović <132358703+Emina89@users.noreply.github.com>
2024-03-14 12:04:27 +0100

    Klijent
    Završna aplikacija C Route

---
+++ Backend/Controllers/KlijentController.cs
@@ -0,0 +1,66 @@
+﻿using Backend.Data;
+using Backend.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class KlijentController
+    {
+        // Dependency injection
+        // Definiraš privatno svojstvo
+        private readonly EdunovaContext _context;
+
+        // Dependency injection
+        // U konstruktoru primir instancu i dodjeliš privatnom svojstvu
+        public KlijentController(EdunovaContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return new JsonResult(_context.Klijenti.ToList());
+        }
+
+        [HttpPost]
+        public IActionResult Post(Klijent klijent)
+        {
+            _context.Klijenti.Add(klijent);
+            _context.SaveChanges();
+            return new JsonResult(klijent);
+        }
+
+        [HttpPut]
+        [Route("{sifra:int}")]
+        public IActionResult Put(int sifra, Klijent klijent)
+        {
+            var smjerIzBaze = _context.Klijenti.Find(sifra);
+            // za sada ručno, kasnije će doći Mapper
+            smjerIzBaze.Ime = klijent.Ime;
+            smjerIzBaze.Prezime = klijent.Prezime;
+            smjerIzBaze.Email = klijent.Email;
+            smjerIzBaze.KontaktBroj =klijent.KontaktBroj;
+
+            _context.Klijenti.Update(smjerIzBaze);
+            _context.SaveChanges();
+
+            return new JsonResult(smjerIzBaze);
+        }
+
+        [HttpDelete]
+        [Route("{sifra:int}")]
+        [Produces("application/json")]
+        public IActionResult Delete(int sifra)
+        {
+            var smjerIzBaze = _context.Klijenti.Find(sifra);
+            _context.Klijenti.Remove(smjerIzBaze);
+            _context.SaveChanges();
+            return new JsonResult(new { poruka="Obrisano"});
+        }
+
+    }
+}
---
+++ Backend/Models/Klijent.cs
@@ -0,0 +1,12 @@
+﻿namespace Backend.Models
+{
+    public class Klijent: Entitet
+    {
+        public string? Ime { get; set; }
+        public string? Prezime { get; set; }
+        public string? Email { get; set; }
+        public string? KontaktBroj { get; set; }
+
+
+    }
+}
